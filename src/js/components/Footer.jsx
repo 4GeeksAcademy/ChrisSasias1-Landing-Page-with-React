378175cb27735e19bb07008d84eeaeca24243681
Christopher Sasias <christophersasias@hotmail.com>
2025-11-06 22:13:45 +0000

    Create Footer

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -7,7 +7,14 @@ export const Footer = () => {
 
     return (
 
-        <div></div>
+        <div>
+            <footer className="bg-dark text-light text-center py-3 w-100 fixed-bottom">
+                <div className="container-fluid">
+                    <p className="mb-0">Copyright © Your Website 2018</p>
+                </div>
+            </footer>
+
+        </div>
 
     )
 }
